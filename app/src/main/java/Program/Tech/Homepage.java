68236a3769f5c08dc9661ab42e3c664d61e28eeb
michael <kunyang.xu@student.kuleuven.be>
2024-05-15 19:35:19 +0200

    14/05/2024

--- app/src/main/java/Program/Tech/Homepage.java
+++ app/src/main/java/Program/Tech/Homepage.java
@@ -1,6 +1,10 @@
 package Program.Tech;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,18 @@ public class Homepage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // 找到 ImageView 对象
+        ImageView imageView = findViewById(R.id.image);
+        // 设置动画资源为 ImageView 的背景
+        imageView.setBackgroundResource(R.drawable.donation_show);
+        // 将背景转换为 AnimationDrawable 对象
+        AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getBackground();
+        // 启动动画
+        animationDrawable.start();
+    }
+
+    public void Test(View view) {
+        startActivity(new Intent(this,Slideview.class));
     }
 }
